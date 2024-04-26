7265984b744e07706907530862a9b9481e7b02fe
ch-teng <cteng601@gmail.com>
2024-04-25 23:50:33 -0400

    Added rough draft menu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,140 @@
-import Image from "next/image";
+"use client";
+import {
+  motion,
+  AnimatePresence,
+  useAnimate,
+  stagger,
+  AnimationSequence,
+  usePresence,
+  MotionConfig,
+} from "framer-motion";
+import { useEffect, useState } from "react";
+
+const MenuItems = () => {
+  const [isPresent, safeToRemove] = usePresence();
+  const [scope, animate] = useAnimate();
+
+  useEffect(() => {
+    if (isPresent) {
+      const startAni = async () =>
+        await animate([
+          [
+            "li",
+            {
+              x: "75%",
+            },
+            {
+              type: "spring",
+              delay: stagger(0.2),
+              at: "-.1",
+            },
+          ],
+        ]);
+      startAni();
+    } else {
+      const endAni = async () => {
+        await animate([
+          [
+            "li",
+            { x: "-25%" },
+            { delay: stagger(0.02, { from: "last" }), at: "<" },
+          ],
+        ]);
+        safeToRemove();
+      };
+      endAni();
+    }
+  }, [isPresent]);
+
+  return (
+    <motion.ul
+      key="menu"
+      initial={{ left: "-40rem" }}
+      animate={{ left: "-5rem" }}
+      exit={{ left: "-40rem" }}
+      ref={scope}
+      className="menu"
+    >
+      <MotionConfig
+        transition={{
+          type: "spring",
+          stiffness: 500,
+        }}
+      >
+        <motion.li initial={{ x: "-5rem" }} whileHover={{ scale: 1.5 }}>
+          whoami
+        </motion.li>
+        <motion.li whileHover={{ scale: 1.5 }}>Experiences</motion.li>
+        <motion.li initial={{ x: "-20%" }} whileHover={{ scale: 1.5 }}>
+          Personal Projects
+        </motion.li>
+        <motion.li whileHover={{ scale: 1.5 }}>Hobbies</motion.li>
+      </MotionConfig>
+    </motion.ul>
+  );
+};
 
 export default function Home() {
+  const [showMenu, setShowMenu] = useState(false);
+  const [scope, animate] = useAnimate();
+  const handleClick = () => {
+    setShowMenu(!showMenu);
+    animate("#top", {
+      rotate: showMenu ? "0deg" : "135deg",
+      y: showMenu ? "0rem" : ".35rem",
+    });
+    animate("#bot", {
+      rotate: showMenu ? "0deg" : "-135deg",
+      y: showMenu ? "0rem" : "-.35rem",
+    });
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <body>
+      <div ref={scope}>
+        <button className="hamburger" onClick={handleClick}>
+          <motion.div
+            className="hamburg 1"
+            id="top"
+            initial={{ y: 0, rotate: 0 }}
+            transition={{ duration: 0.5, ease: "backInOut" }}
+            layout
+          />
+          <AnimatePresence initial={false}>
+            {!showMenu && (
+              <motion.div
+                className="hamburg 2"
+                initial={{ scale: 0 }}
+                animate={{ scale: 1 }}
+                exit={{ scale: 0 }}
+                transition={{ duration: 0.1, ease: "backInOut" }}
+              />
+            )}
+          </AnimatePresence>
+
+          <motion.div
+            className="hamburg 3"
+            id="bot"
+            initial={{ y: 0, rotate: 0 }}
+            transition={{ duration: 0.5, ease: "backInOut" }}
+            layout
+          />
+        </button>
+        <AnimatePresence>
+          {showMenu && (
+            <motion.div
+              key="menuCover"
+              className="menuCover"
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 0.5 }}
+              exit={{ opacity: 0 }}
+              transition={{ duration: 0.3, ease: "anticipate" }}
+            ></motion.div>
+          )}
+        </AnimatePresence>
+
+        <AnimatePresence>{showMenu && <MenuItems />}</AnimatePresence>
       </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </body>
   );
 }
