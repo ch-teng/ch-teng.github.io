574b45b4fc2c62b360719886dd5b576af4fb18e2
ch-teng <cteng601@gmail.com>
2024-04-27 10:55:16 -0400

    Basic css and animation for Experiences

--- src/components/ExperienceScroller.tsx
+++ src/components/ExperienceScroller.tsx
@@ -1,5 +1,6 @@
-import { closestItem, removeItem } from "@/app/utils/array-utils";
+import { closestItem, removeItem } from "@/utils/array-utils";
 import { AnimatePresence, Reorder, motion } from "framer-motion";
+import Image from "next/image";
 import { useState } from "react";
 
 interface Job {
@@ -7,6 +8,8 @@ interface Job {
   company: string;
   date: string;
   description: string;
+  imgSrc: string;
+  imgDesc: string;
 }
 
 const experience = [
@@ -15,30 +18,40 @@ const experience = [
     company: "Priceline",
     date: "Jan 2021 - June 2021",
     description: "Priceline Job",
+    imgSrc: "/priceline-logo.png",
+    imgDesc: "Priceline Logo",
   },
   {
     title: "Web Developer Coop",
     company: "TriNetX",
     date: "May 2020 - Aug 2020",
     description: "TriNetX Job",
+    imgSrc: "/TriNetX-Logo-PR.jpeg",
+    imgDesc: "TriNetX Logo",
   },
   {
     title: "Software Developer",
     company: "Hasbro",
     date: "Jan 2021 - June 2021",
     description: "Hasbro Software Developer Job",
+    imgSrc: "/Hasbro_logo_symbol.png",
+    imgDesc: "Hasbro Logo",
   },
   {
     title: "Project Developer",
     company: "PrepareRI",
     date: "Jan 2021 - June 2021",
     description: "Project Developer",
+    imgSrc: "/PrepareRI_logo.png",
+    imgDesc: "PrepareRI Logo",
   },
   {
     title: "Cyber Security Analyst Intern",
     company: "Hasbro",
     date: "Jan 2021 - June 2021",
     description: "Hasbro Cyber Security Analyst Intern Job",
+    imgSrc: "/Hasbro_logo_symbol.png",
+    imgDesc: "Hasbro Logo",
   },
 ];
 
@@ -51,7 +64,7 @@ export default function ExperienceScroller() {
     setJobs([job, ...temp]);
   };
   return (
-    <div className="page Experience">
+    <div className="page experience">
       <Reorder.Group
         className="scroller"
         as="ul"
@@ -68,6 +81,7 @@ export default function ExperienceScroller() {
               initial={{ opacity: 0, y: 20 }}
               animate={{ opacity: 1, y: 0 }}
               exit={{ opacity: 0, y: -20 }}
+              dragListener={false}
             >
               <motion.button
                 whileHover={{ scale: 1.1 }}
@@ -78,7 +92,14 @@ export default function ExperienceScroller() {
                   handleJobClick(job);
                 }}
               >
-                {job.title}
+                <Image
+                  src={job.imgSrc}
+                  alt="hasbro"
+                  width={50}
+                  height={50}
+                  className="rounded"
+                />
+                <div>{job.title}</div>
               </motion.button>
             </Reorder.Item>
           ))}
